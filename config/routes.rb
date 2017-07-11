c567be9fa3c520a730918bfd91ddf0af0871ba74
Neil Williams <neil.williams227@gmail.com>
2017-07-11 18:23:24 -0500

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  
   resources :posts do
   	resources :comments
   end
 
   root 'posts#index'
+  get '/about', to: 'pages#about'
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
