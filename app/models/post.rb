cff7030779d2ebe4d39bd532ec8cc17b14e02e24
Neil Williams <neil.williams227@gmail.com>
2017-07-11 18:15:42 -0500

    added comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+	has_many  :comments, dependent: :destroy
+	validates :headline, presence: true, length: {minimum: 5}
+	validates :questionone,  presence: true
+	validates :questiontwo,  presence: true
+	validates :questionthree,  presence: true
+	validates :descriptionone,  presence: true
+	validates :descriptiontwo,  presence: true
+	validates :descriptionthree,  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'welcome/index'
+  
+  resources :posts do
+  	resources :comments
+  end
 
-  root 'welcome#index'
+  root 'posts#index'
+  get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
